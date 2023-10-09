73d68405e73de74c7b8aa5d0ca3118cf35dd013c
yukkuriremiria <98326743+yukkuriremiria@users.noreply.github.com>
2023-10-09 21:02:58 +0900

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
 
 const proxyOptionsForApp1 = {
-  target: 'http://localhost:3000/index.html',  // /app1 パスへのリクエストをここに転送
+  target: 'http://localhost:3000/',  // / パスへのリクエストをここに転送
   changeOrigin: true,
   logLevel: 'debug',
 };
@@ -18,7 +18,7 @@ const proxyOptionsForApp2 = {
 
 // パスに基づいて異なるリバースプロキシを設定
 app.use('/', createProxyMiddleware(proxyOptionsForApp1));
-app.use('/ytdl', createProxyMiddleware(proxyOptionsForApp2));
+app.use('/app2', createProxyMiddleware(proxyOptionsForApp2));
 
 
 const IP_ADDRESS = 'localhost';
